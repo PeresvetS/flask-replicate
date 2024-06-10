988458b379e9d8b649054604c7340524eb185e09
Nikita Solovev <nik.writter@ya.ru>
2024-06-10 22:58:09 +0300

    add logger

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import logging
 from flask import Flask
 from waitress import serve
@@ -7,7 +8,17 @@ from split_text import split_text_blueprint
 
 def create_app():
     app = Flask(__name__)
-    app.logger.setLevel(logging.DEBUG) 
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_handler.setFormatter(formatter)
+    stream_handler.setLevel(logging.DEBUG)
+
+    app.logger.addHandler(stream_handler)
+
+    root_logger = logging.getLogger()
+    root_logger.addHandler(stream_handler)
+
 
     # Зарегистрируйте маршруты
     app.register_blueprint(generate_blueprint)
