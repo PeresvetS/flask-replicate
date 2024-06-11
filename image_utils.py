5bdab56961821f5e5a149bd2dbe40a3b5d35ae01
Nikita Solovev <nik.writter@ya.ru>
2024-06-11 21:05:36 +0300

    fix text match

--- image_utils.py
+++ image_utils.py
@@ -12,7 +12,7 @@ def add_footer_with_text_and_squares(image_path, colors_and_text):
 
     # Найти текстовую часть
     text_match = re.search(r'[a-zA-Z]+.', colors_and_text)
-    text = text_match.group(1).strip() if text_match else ""
+    text = text_match if text_match else ""
     
     # Получить базовый путь из переменной окружения
     base_path = os.getenv("RAILWAY_VOLUME_MOUNT_PATH", "/app/data")
@@ -48,7 +48,7 @@ def add_footer_with_text_and_squares(image_path, colors_and_text):
         draw.rectangle([position, (position[0] + square_size, position[1] + square_size)], fill=color)
 
     # Добавить текст
-    font = ImageFont.load_default(size=20)
+    font = ImageFont.load_default(size=45)
     text_position = (20, height + 40)  # Поднято вверх
     draw.text(text_position, text, fill=(0, 0, 0), font=font)
 
